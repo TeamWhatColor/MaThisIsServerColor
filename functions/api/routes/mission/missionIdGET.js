1699b80c94c34d6e3ef0d16230b764186c01b95d
realwhyjay <brandon94_@naver.com>
2021-11-21 03:14:02 +0900

    ✨ [FEAT] : 미션 뷰, 메인 뷰 완성

---
+++ functions/api/routes/mission/missionIdGET.js
@@ -0,0 +1,29 @@
+const _ = require('lodash');
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { missionDB } = require('../../../db');
+
+module.exports = async (req, res) => {
+  const { id } = req.params;
+  // request params가 잘못됐을 때
+  if (!id) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+
+  let client;
+
+  try {
+    client = await db.connect(req);
+    const data = await missionDB.getMission(client, id);
+    console.log(data);
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_USERS_SUCCESS, data));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
---
+++ functions/db/mission.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
+
+const getMission = async (client, id) => {
+  const { rows } = await client.query(
+    `
+    SELECT m.content
+    FROM "user" u JOIN team t
+        on u.team_id = t.team_id
+     JOIN mission m
+        on t.mission_id = m.mission_id
+    WHERE u.id = $1
+        `,
+    [id],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
+module.exports = { getMission };
